365d6af695e480cad98e9758e66c06af7d58c011
Darya <daryabich98@gmail.com>
2020-03-26 14:35:28 +0300

    Add Entities and Dao

---
+++ src/main/java/org/StartApplication/Application.java
@@ -0,0 +1,7 @@
+package org.StartApplication;
+
+public class Application {
+    public static  void main (String[] args){
+
+    }
+}
---
+++ src/main/java/org/StartApplication/ApplicationFacade.java
@@ -0,0 +1,4 @@
+package org.StartApplication;
+
+public class ApplicationFacade {
+}
