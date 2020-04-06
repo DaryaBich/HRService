52e3bb7369cd9e738dd0bcdc0e5de1aa2eec0ea7
Darya <daryabich98@gmail.com>
2020-04-06 13:08:52 +0300

    fix adding from file

--- src/main/java/org/controllers/AddFromFileOperationController.java
+++ src/main/java/org/controllers/AddFromFileOperationController.java
@@ -6,16 +6,21 @@ import org.dao.*;
 import org.entities.Department;
 import org.entities.Employee;
 import org.entities.Position;
+import org.utils.JsonUtilsDataExtractor;
 import org.utils.XmlUtilsDataExtractor;
 
 import java.util.List;
 
 public class AddFromFileOperationController implements OperationTypeController {
+    // add from file/department/cite
     @Override
     public String execute(String[] parseCommands, DepartmentDao departmentDao) {
         StringBuilder added = new StringBuilder("Added id: ");
         StringBuilder notAdded = new StringBuilder("Not added id: ");
-        List<Department> departments = ApplicationContext.getDataAccessor().extractDepartments();
+        List<Department> departments = ApplicationContext.getDataAccessor().getProperties().getProperty("suffix")
+                .equals("xml") ?
+                XmlUtilsDataExtractor.extractDepartments(parseCommands[2]) :
+                JsonUtilsDataExtractor.extractDepartments(parseCommands[2]);
         for (Department department : departments) {
             if (departmentDao.addDepartment(department.getName(), department.getChiefId())) {
                 added.append(department.getId());
@@ -30,31 +35,37 @@ public class AddFromFileOperationController implements OperationTypeController {
     public String execute(String[] parseCommands, EmployeeDao employeeDao) {
         StringBuilder added = new StringBuilder("Added id: ");
         StringBuilder notAdded = new StringBuilder("Not added id: ");
-        List<Employee> employees = ApplicationContext.getDataAccessor().extractEmployees();
-            for (Employee employee : employees) {
-                if (employeeDao.addEmployee(employee.getFIO(), employee.getIdDepartment(), employee.getPhoneNumber(),
-                        employee.getSeniority(), employee.getIdPosition())) {
-                    added.append(employee.getId());
-                } else {
-                    notAdded.append(employee.getId());
-                }
+        List<Employee> employees = ApplicationContext.getDataAccessor().getProperties().getProperty("suffix")
+                .equals("xml") ?
+                XmlUtilsDataExtractor.extractEmployees(parseCommands[2]) :
+                JsonUtilsDataExtractor.extractEmployees(parseCommands[2]);
+        for (Employee employee : employees) {
+            if (employeeDao.addEmployee(employee.getFIO(), employee.getIdDepartment(), employee.getPhoneNumber(),
+                    employee.getSeniority(), employee.getIdPosition())) {
+                added.append(employee.getId());
+            } else {
+                notAdded.append(employee.getId());
             }
-            return added.toString() + "\n" + notAdded.toString();
+        }
+        return added.toString() + "\n" + notAdded.toString();
     }
 
     @Override
     public String execute(String[] parseCommands, PositionDao positionDao) {
         StringBuilder added = new StringBuilder("Added id: ");
         StringBuilder notAdded = new StringBuilder("Not added id: ");
-        List<Position> positions = ApplicationContext.getDataAccessor().extractPositions();
-            for (Position position : positions) {
-                if (positionDao.addPosition(position.getName(), position.getSalary())) {
-                    added.append(position.getId());
-                } else {
-                    notAdded.append(position.getId());
-                }
+        List<Position> positions = ApplicationContext.getDataAccessor().getProperties().getProperty("suffix")
+                .equals("xml") ?
+                XmlUtilsDataExtractor.extractPositions(parseCommands[2]) :
+                JsonUtilsDataExtractor.extractPositions(parseCommands[2]);
+        for (Position position : positions) {
+            if (positionDao.addPosition(position.getName(), position.getSalary())) {
+                added.append(position.getId());
+            } else {
+                notAdded.append(position.getId());
             }
-            return added.toString() + "\n" + notAdded.toString();
+        }
+        return added.toString() + "\n" + notAdded.toString();
     }
 
 }
